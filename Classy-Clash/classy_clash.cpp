483f46b93a60e7eccad7a2217f4bd2440755cd50
EvelineGomes <gomes.evepg@gmail.com>
2023-02-09 08:41:42 -0500

    drew and scale the knight

--- Classy-Clash/classy_clash.cpp
+++ Classy-Clash/classy_clash.cpp
@@ -19,7 +19,7 @@ int main() {
    float speed = 4.0;
 
    // load the knight character
-   Texture2D knight = LoadTexture("characters/knight_idle.spritesheet.png");
+   Texture2D knight = LoadTexture("characters/knight_idle_spritesheet.png");
    Vector2 knightPos{
       (float)winDimensions[0] / 2.0f - 4.0f * (0.5f * (float)knight.width / 6.0f), // windows width - half of knight.width divided by 6 as the sprite sheet as 6 images.
       (float)winDimensions[1] / 2.0f - 4.0f * (0.5f * (float)knight.height)
@@ -35,18 +35,25 @@ int main() {
       
       // vector and keys to determine the direction among the map
       Vector2 direction{};
-      if (IsKeyDown(KEY_A)) direction.x -= 1.0;
-      if (IsKeyDown(KEY_D)) direction.x += 1.0;
-      if (IsKeyDown(KEY_W)) direction.y -= 1.0;
-      if (IsKeyDown(KEY_S)) direction.y += 1.0;
+      if (IsKeyDown(KEY_A)) direction.x -= 1.0f;
+      if (IsKeyDown(KEY_D)) direction.x += 1.0f;
+      if (IsKeyDown(KEY_W)) direction.y -= 1.0f;
+      if (IsKeyDown(KEY_S)) direction.y += 1.0f;
 
       // direction vector used to move the map:
-      if (Vector2Length(direction) != 0.0) {
+      if (Vector2Length(direction) != 0.0f) {
          mapPos = Vector2Subtract(mapPos, Vector2Scale(Vector2Normalize(direction), speed));
       }
 
       // draw the map
-      DrawTextureEx(map, mapPos, 0.0, 4.0, WHITE);
+      DrawTextureEx(map, mapPos, 0.0f, 4.0f, WHITE);
+
+      // draw the character
+      // 1st. create source and dest rectangles
+      Rectangle srcK{0.f, 0.f, (float)knight.width/6.f, (float)knight.height};
+      // for the dest rectangle, we'll multiply the width and height by 4.0 to scale the image
+      Rectangle destK{knightPos.x, knightPos.y, 4.0f * (float)knight.width/6.0f, 4.0f * (float)knight.height};
+      DrawTexturePro(knight, srcK, destK, Vector2{}, 0.f, WHITE); // for the Vector2 origin parameter, we pass a Vector2{}. This way, using curly braces initialization, all variables are set to 0 in that Vector2.
 
       EndDrawing();
    }
