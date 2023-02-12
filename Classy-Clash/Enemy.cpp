5ad2fbcb2b58c56eac8862610dc4ead3b52f4ab6
EvelineGomes <gomes.evepg@gmail.com>
2023-02-12 13:50:53 -0500

    added the basic Enemy AI code from pseudocode

--- Classy-Clash/Enemy.cpp
+++ Classy-Clash/Enemy.cpp
@@ -10,19 +10,18 @@ Enemy::Enemy(Vector2 pos, Texture2D idle_texture, Texture2D run_texture) {
    m_run = run_texture;
    m_width = m_texture.width / m_maxFrames;
    m_height = m_texture.height;
+   m_speed = 3.5f;
 }
 
 void Enemy::tick(float deltaTime) {
-   // pseudocode to move the enemy towards the character
    // 1. get toTarget vector value
-   //       toTarget = character's screen position vector - enemy's screen position vector
+   Vector2 toTarget = Vector2Subtract(m_target->getScreenPos(), m_screenPos);
    // 2. normalize toTarget
-   //       use Vector2Normalize
+   toTarget = Vector2Normalize(toTarget);
    // 3. multiply toTarget by speed
-   //       use Vector2Scale passing toTarget and a speed value
-   // 4. move Enemy by setting the m_worldPos
-   //       update m_worldPos by setting it to the value of toTarget
-
+   toTarget = Vector2Scale(toTarget, m_speed);
+   // 4. move Enemy by ADDing m_worldPos and toTarget
+   m_worldPos = Vector2Add(m_worldPos, toTarget);
 
    // set m_screenPos before calling tick function because m_screenPos is used/updated there to draw the enemy to the screen
    m_screenPos = Vector2Subtract(m_worldPos, m_target->getWorldPos());
