21035c2b5b674c338b7a71a8562076cbcfa56781
EvelineGomes <gomes.evepg@gmail.com>
2023-02-12 13:41:01 -0500

    added screenPos getter in Character class to use in Enemy tick()

--- Classy-Clash/Character.h
+++ Classy-Clash/Character.h
@@ -10,6 +10,7 @@ public:
    Character();
    Character(int winWidth, int winHeight);
    Vector2 getWorldPos() { return m_worldPos; }
+   Vector2 getScreenPos() { return m_screenPos; }
    virtual void tick(float deltaTime);
 };
 #endif // !CHARACTER_H
