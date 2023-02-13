270d9dd046063d57b24f4173c0ebcb29ca51ada3
EvelineGomes <gomes.evepg@gmail.com>
2023-02-13 17:15:00 -0500

    added rotation local variable in tick()

--- Classy-Clash/Character.cpp
+++ Classy-Clash/Character.cpp
@@ -28,47 +28,43 @@ void Character::tick(float deltaTime) {
 
    Vector2 origin{};
    Vector2 offset{};
+   float rotation{};
    if (m_rightLeft > 0.f) {
       origin = { 0.f, m_weapon.height * m_scale };
       offset = { 38.f, 55.f };
+      
       m_weaponCollisionRec = {
          getScreenPos().x + offset.x,
          getScreenPos().y + offset.y - m_weapon.height * m_scale,
          m_weapon.width * m_scale,
          m_weapon.height * m_scale
       };
+      rotation = 35.f;
    }
    else {
       origin = { m_weapon.width * m_scale, m_weapon.height * m_scale };
-      offset = { 25.f, 55.f };
+      offset = { 25.f, 55.f };      
+      
       m_weaponCollisionRec = {
-         getScreenPos().x + offset.x - m_weapon.width * m_scale,
+         getScreenPos().x + offset.x,
          getScreenPos().y + offset.y - m_weapon.height * m_scale,
          m_weapon.width * m_scale,
          m_weapon.height * m_scale
       };
+      rotation = -35.f;
    }
 
    // draw the sword
    Rectangle source{ 0.f, 0.f, static_cast<float>(m_weapon.width) * m_rightLeft, static_cast<float>(m_weapon.height) };
    Rectangle dest{ getScreenPos().x + offset.x, getScreenPos().y + offset.y, m_weapon.width * m_scale, m_weapon.height * m_scale };
-   DrawTexturePro(m_weapon, source, dest, origin, 0.f, WHITE);
+   DrawTexturePro(m_weapon, source, dest, origin, rotation, WHITE);
+   // due to setting the origin vector to the bottom left and bottom right corners, the sword is "facing" down
 
-   // using the following function, it's possible to see the Rectangle's lines! In this case we're passing the dest Rectangle information
    DrawRectangleLines(
-      //getScreenPos().x + offset.x, getScreenPos().y + offset.y, m_weapon.width * m_scale, m_weapon.height * m_scale, RED
-      // now instead of the dest Rec, we'll use the m_weaponCollisionRec
       m_weaponCollisionRec.x,
       m_weaponCollisionRec.y,
       m_weaponCollisionRec.width,
       m_weaponCollisionRec.height,
       RED
    );
-   /**
-   * the rectangle is below the sword and that's because we've set it to be in the bottom;
-   * so the thing to do now is to move the rectangle up by the height of the texture when the character is facing right, and when facing left we'll also need to move left by the width of the weapon texture
-   * 
-   * Another rectangle member will be added to this Character class called m_weaponCollisionRec that'll be set in this tick function
-   */
-   
 }
